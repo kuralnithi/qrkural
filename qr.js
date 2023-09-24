4194e849c8372cda4c6b4c265a32e80791dd5283
kuranithi <kuralnithi1999@gmail.com>
2023-09-24 14:05:33 +0530

    chages updated

--- qr.js
+++ qr.js
@@ -1,29 +1,55 @@
 const generateBtn = document.getElementById('generate');
-const downloadLink = document.getElementsByClassName('download')[0];
-    
+    const downloadLink = document.getElementsByClassName('download')[0];
+        
 
-        const eData =  document.getElementById('data');
-        const width =  document.getElementById('width');
-        const height =  document.getElementById('height');
-        const qrcolor =  document.getElementById('qr-color');
-        const bgcolor =  document.getElementById('bg-color');
-        const format =  document.getElementById('format');
+            const eData =  document.getElementById('data');
+            const width =  document.getElementById('width');
+            const height =  document.getElementById('height');
+            const qrcolor =  document.getElementById('qr-color');
+            const bgcolor =  document.getElementById('bg-color');
+            const format =  document.getElementById('format');
 
-const qrGen =  () => {
-    
-    const qrimg = "https://api.qrserver.com/v1/create-qr-code/?data=" + eData.value +"&size=" + height.value +"x"+ width.value + "&format=" + format.value + "&color=" + qrcolor.value + "&bgcolor="+ bgcolor.value ;
+let qrimg;
 
-  const imgbox = document.getElementsByClassName('imgbox')[0];
-  imgbox.src = qrimg;   
-    downloadLink.href =  qrimg ;
-    
-    downloadLink.download = `qrcode.${format.value}`;
-    downloadLink.style.display = "block";
-    console.log(qrimg);
-    console.log(eData.value, width.value, height.value, qrcolor.value, bgcolor.value, format.value);
-};
+    const qrGen =  () => {
+        downloadLink.style.display = "block";
+      
 
-const downloadClk = () => {
-  downloadLink.href.click();
-  console.log(downloadLink.click());
-}
+        qrimg = "https://api.qrserver.com/v1/create-qr-code/?data=" + eData.value +"&size=" + height.value +"x"+ width.value + "&format=" + format.value + "&fgcolor=" + qrcolor.value + "&bgcolor="+ bgcolor.value ;
+
+        const imgbox = document.getElementsByClassName('imgbox')[0];
+        imgbox.src = qrimg;   
+
+      
+
+        generateBtn.disabeled=true
+
+        // console.log(qrimg);
+        // console.log(eData.value, width.value, height.value, qrcolor.value, bgcolor.value, format.value);
+
+    };
+
+
+
+    const downloadClk = async() => {
+      try {
+        const responce = await fetch(qrimg);
+        const rblob =  await responce.blob();
+      
+        const objUrl = window.URL.createObjectURL(rblob);
+        
+        downloadLink.href = objUrl;
+        downloadLink.download=`qrcode.${format.value}`
+        downloadLink.click();
+        window.URL.revokeObjectURL(objUrl);
+
+        
+        downloadLink.removeEventListener
+        location.reload();
+      } catch (error) {
+        console.error(`THI IS THE ERROR---->   ${error}`);
+      }
+      
+    }
+downloadLink.addEventListener('click', downloadClk);
+generateBtn.addEventListener('click', qrGen);
