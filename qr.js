549c933cac204f817b1325a7ed4afdc3c64e7156
kuranithi <kuralnithi1999@gmail.com>
2023-10-13 10:21:42 +0530

    qrcolor removed

--- qr.js
+++ qr.js
@@ -2,20 +2,20 @@ const generateBtn = document.getElementById('generate');
     const downloadLink = document.getElementsByClassName('download')[0];
         
 
-            const eData =  document.getElementById('data');
+            const eData =  document.getElementById("data");
             const width =  document.getElementById('width');
             const height =  document.getElementById('height');
-            const qrcolor =  document.getElementById('qr-color');
             const bgcolor =  document.getElementById('bg-color');
             const format =  document.getElementById('format');
 
 let qrimg;
 
-    const qrGen =  () => {
+const qrGen = () => {
+      
         downloadLink.style.display = "block";
       
 
-        qrimg = "https://api.qrserver.com/v1/create-qr-code/?data=" + eData.value +"&size=" + height.value +"x"+ width.value + "&format=" + format.value + "&fgcolor=" + qrcolor.value + "&bgcolor="+ bgcolor.value ;
+  qrimg = "https://api.qrserver.com/v1/create-qr-code/?data=" + eData.value + "&size=" + height.value + "x" + width.value + "&format=" + format.value + "&bgcolor=" + bgcolor.value;
 
         const imgbox = document.getElementsByClassName('imgbox')[0];
         imgbox.src = qrimg;   
@@ -38,13 +38,13 @@ let qrimg;
       
         const objUrl = window.URL.createObjectURL(rblob);
         
-        downloadLink.href = objUrl;
+        downloadLink.href =   objUrl;
         downloadLink.download=`qrcode.${format.value}`
         downloadLink.click();
         window.URL.revokeObjectURL(objUrl);
-
         
-        downloadLink.removeEventListener
+        downloadLink.removeEventListener('click', downloadClk);
+
         location.reload();
       } catch (error) {
         console.error(`THI IS THE ERROR---->   ${error}`);
